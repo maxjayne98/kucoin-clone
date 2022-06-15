1272b3210554320ff8af334aaf46ae17c7ad02ef
mahdi <mahdi.jax@gmail.com>
2022-06-16 02:55:55 +0430

    feat:add kucoin color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('./styles/colors');
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors,
     extend: {},
   },
   plugins: [],
