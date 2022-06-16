994cfffa3a829dfe3f94046b9d7905e2dcf5f367
himmelio <safari.sepehr@gmail.com>
2022-06-16 12:12:23 +0430

    style: convert double quotes and add semicolons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ const colors = require('./styles/colors');
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors,
     extend: {},
   },
   plugins: [],
-}
+};
