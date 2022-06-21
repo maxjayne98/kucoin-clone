e91329d4099326643ee691f6db93c4af69b91454
himmelio <safari.sepehr@gmail.com>
2022-06-22 01:17:00 +0430

    feat: add dots icon

---
+++ components/Shared/Svg/Icons/DotsVerticalIcon.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+
+import { Props } from './types';
+
+const DotsVerticalIcon = ({ className }: Props) => {
+  const cn = classNames('h-5 w-5', className);
+
+  return (
+    <svg
+      className={cn}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+      />
+    </svg>
+  );
+};
+
+export default DotsVerticalIcon;
--- components/Shared/Svg/Icons/index.ts
+++ components/Shared/Svg/Icons/index.ts
@@ -1,2 +1,3 @@
 export { default as ArrowDownIcon } from './ArrowDownIcon';
 export { default as GlobeIcon } from './GlobeIcon';
+export { default as DotsVerticalIcon } from './DotsVerticalIcon';
